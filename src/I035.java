14dc0e0b7b90e7d22a6ee28fa63fe48d6a36bcef
OneSoil <414528491@qq.com>
2019-10-29 10:00:53 +0800

    10/29

---
+++ src/I035.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * @author Mr.wang
+ * @date 2019-10-29 08:13
+ * function_:
+ * summary
+ * *    1.使用BufferedReader比Scanner时间效率高很多，但可能内存上可能会超出
+ * *    2.两点间的距离一共有两种情况，两种情况之和为总距离
+ * *    3.算出从小到大的距离，另一个距离为总距离减此距离，比较两种距离的大小
+ */
+public class I035 {
+    public static void main(String[] args)throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i;
+        int[] distence = new int[n];
+        int total = 0;
+        for(i = 0; i < n; i++){
+            distence[i] = sc.nextInt();
+            total +=distence[i];
+        }
+
+        int m = sc.nextInt();
+
+        for(i = 0; i < m; i++){
+            int flag;
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a > b){
+                flag = a;
+                a = b;
+                b = flag;
+            }
+            int result = 0;
+            while (a < b){
+                result += distence[a-1];
+                a++;
+            }
+            System.out.println(result > total/2 ? total - result : result);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        int str = 14;
-        System.out.println("wang"+str % 12);
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
 
     }
 }

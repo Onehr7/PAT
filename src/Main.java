45f622244488db1e8d9ea362f7c754f21b5b6014
OneSoil <414528491@qq.com>
2019-10-31 22:01:50 +0800

    1060有毒

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
+import java.util.Arrays;
+
 public class Main{
     public static void main(String[] args){
-        int b = 'a';
-        char a =(char) (b -32);
-        int c = 'z';
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(a);
-        System.out.println((int)'A');
-        System.out.println((int)'Z');
 
-        int d = 1;
+        String a = "wang";
+        String b = a.replace('a','w');
+        System.out.println(a);
+        System.out.println(b);
+
     }
 }

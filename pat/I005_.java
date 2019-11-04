459e1179644a584cb609e489241196bb149f968e
OneSoil <414528491@qq.com>
2019-11-04 11:29:41 +0800

    pat 1005

---
+++ pat/I005_.java
@@ -0,0 +1,58 @@
+import java.io.*;
+/**
+ * @author Mr.wang
+ * @date 2019-11-04 11:11
+ * function_:
+ */
+public class I005_ {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num = br.readLine();
+        long total = 0;
+        for(int i = 0; i < num.length(); i++){
+            total += Integer.parseInt(num.charAt(i)+"");
+        }
+        num = total +"";
+        System.out.print(numToString(num.charAt(0)));
+        for(int i = 1; i < num.length(); i++){
+            System.out.print(" "+numToString(num.charAt(i)));
+        }
+    }
+    public static String numToString(char num){
+        switch (num){
+            case '1':
+                return "one";
+
+            case '2':
+                return "two";
+
+            case '3':
+                return "three";
+
+            case '4':
+                return "four";
+
+            case '5':
+                return "five";
+
+            case '6':
+                return "six";
+
+            case '7':
+                return "seven";
+
+            case '8':
+                return "eight";
+
+            case '9':
+                return "nine";
+
+            case '0':
+                return "zero";
+
+            default:
+                return "";
+        }
+
+    }
+}
---
+++ pat/Main_.java
@@ -0,0 +1,12 @@
+/**
+ * @author Mr.wang
+ * @date 2019-10-31 22:15
+ * function_:
+ */
+public class Main_ {
+    public static void main(String[] args){
+        char a = '5';
+        int b = Integer.parseInt(a+"");
+        System.out.println(b);
+    }
+}
